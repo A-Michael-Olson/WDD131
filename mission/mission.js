a95b020de952f2796cd9e21d4f1297bff6d7252f
Mike Olson <anthonymolson@hotmail.com>
2026-01-16 17:31:15 -0700

    fixed issues within JS and CSS file

--- mission/mission.js
+++ mission/mission.js
@@ -1,18 +1,18 @@
 const themeSelect = document.getElementById('theme');
 const body = document.body;
-const logoIMG = document.getElementById('.logo img');
+const logoIMG = document.querySelector('.logo img');
 
 
 themeSelect.addEventListener('change', function () {
     const theme = themeSelect.value;
 
     if (theme === 'dark') {
-        body.classList.add('dark-theme');
+        body.classList.add('dark');
         logoIMG.src = 'assets/byui-logo-white.webp';
     }
 
     if (theme === 'light') {
-        body.classList.remove('dark-theme');
+        body.classList.remove('dark');
         logoIMG.src = 'assets/byui-logo.webp';
     }
 });
