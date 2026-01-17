5fab26d7cbdf05f4c85a80e34ca8a57625244c98
Mike Olson <anthonymolson@hotmail.com>
2026-01-17 08:53:09 -0700

    I think the block scope line was breaking the js and not outputting anything

--- week1.js
+++ week1.js
@@ -1,18 +1,24 @@
-
 const PI = 3.14;
 let radius = 3;
 
 const one = 1;
 const two = '2';
 
-let course = "CSE131"; //global scope
+let course = "CSE131"; // global scope
 if (true) {
     let student = "John";
-    console.log(course);  //works just fine, course is global
-    console.log(student); //works just fine, it's being accessed within the block
+    console.log(course);   // works
+    console.log(student); // works
 }
-console.log(course); //works fine, course is global
-console.log(student); //does not work, can't access a block variable outside the block
+
+console.log(course); // works
+
+try {
+    console.log(student);
+} catch (error) {
+    console.log("student is not accessible outside the block");
+}
+
 console.log(PI);
 console.log(radius);
 console.log(one);
