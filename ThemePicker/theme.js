db810fd983584caddd5b4d65967263a8ba52c378
Mike Olson <anthonymolson@hotmail.com>
2026-01-17 09:04:48 -0700

    create the theme switcher page

---
+++ ThemePicker/theme.js
@@ -0,0 +1,22 @@
+let selectElem = document.querySelector('#theme-select');
+let pageContent = document.querySelector('body');
+
+selectElem.addEventListener('change', changeTheme);
+
+function changeTheme() {
+    let current = selectElem.value;
+    if (current === 'ocean') {
+        document.body.style.backgroundImage = 'ocean.jpeg';;
+        pageContent.style.fontFamily = "Papyrus, fantasy";
+    } else if (current === 'forest') {
+        document.body.style.backgroundImage = 'forest.jpeg';
+        pageContent.style.fontFamily = "Impact, sans-serif";
+    } else if (current === 'desert') {
+        document.body.style.backgroundImage = 'desert.jpeg';
+        pageContent.style.fontFamily = "'Big Caslon', serif";
+    } else {
+        // default
+        document.body.style.backgroundImage = "none";
+        pageContent.style.fontFamily = "Georgia, serif";
+    }
+}
