e2a98296256af950a2ae76b41ae6fd9bbf7f4f87
AMichaelOlson <anthonymolson@byui.edu>
2026-01-27 15:04:47 -0700

    Added the menu toggle functionality

--- W03/Ponder/responsive.js
+++ W03/Ponder/responsive.js
@@ -1,1 +1,10 @@
+var menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
 
+
+menuBtn.addEventListener('click', toggleMenuButton);
+
+function toggleMenuButton() {
+    menu.classList.toggle('hide');
+    menuBtn.classList.toggle('change');
+}
