d30ccad6ffb7a2f2b22068465d7a17e716dbd244
Mike Olson <anthonymolson@hotmail.com>
2026-02-25 21:15:31 -0700

    Completed in class

---
+++ W08/ponder.js
@@ -0,0 +1,44 @@
+const aCourse = {
+    code: 'CSE121b',
+    name: 'Javascript Language',
+    logo: 'images/js-logo.png',
+    sections: [
+        { sectionNum: 1, roomNum: 'STC 353', enrolled: 26, days: 'TTh', instructor: 'Bro T' },
+        { sectionNum: 2, roomNum: 'STC 347', enrolled: 28, days: 'TTh', instructor: 'Sis A' },
+        { sectionNum: 3, roomNum: 'STC 361', enrolled: 14, days: 'MW', instructor: 'Study Hall' }
+    ],
+    enrollStudent: function (sectionNum) {
+        const sectionIndex = this.sections.findIndex(
+            (section) => section.sectionNum == sectionNum
+        );
+        if (sectionIndex >= 0) {
+            this.sections[sectionIndex].enrolled++;
+            renderSections(this.sections);
+        }
+    }
+};
+
+function sectionTemplate(section) {
+    return `<tr>
+      <td>${section.sectionNum}</td>
+      <td>${section.roomNum}</td>
+      <td>${section.enrolled}</td>
+      <td>${section.days}</td>
+      <td>${section.instructor}</td></tr>`
+}
+
+function renderSections(sections) {
+    const html = sections.map(sectionTemplate);
+    document.querySelector("#sections").innerHTML = html.join("");
+}
+
+renderSections(aCourse.sections);
+const courseName = document.querySelector("#courseName");
+courseName.textContent = aCourse.name;
+document.querySelector("#courseCode").textContent = aCourse.code;
+document.querySelector("#course_logo").src = aCourse.logo;
+
+document.querySelector("#enrollStudent").addEventListener("click", function () {
+    const sectionNum = document.querySelector("#sectionNumber").value;
+    aCourse.enrollStudent(sectionNum);
+});
